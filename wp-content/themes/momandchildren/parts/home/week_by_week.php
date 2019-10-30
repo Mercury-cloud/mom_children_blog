822016179873255532da3966aa6ca5ce9cbc6a55
Mercury <drydev033@gmail.com>
2019-10-30 04:00:55 +0200

    added pergenacy letters

--- wp-content/themes/momandchildren/parts/home/week_by_week.php
+++ wp-content/themes/momandchildren/parts/home/week_by_week.php
@@ -21,18 +21,34 @@
         <div class="col-md-9">
             <!-- form -->
             <form action="/page/subscribe-news-letters-pergenancy" method="post">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="date">
+                            <input type="number" id="month" name="MM" placeholder="MM" required>
+                            <input type="number" id="day"   name="DD" placeholder="DD" required>
+                            <input type="number" id="year"  name="YYYY" placeholder="YYYY" required>
+                        </div>
+                    </div>
 
-                <div class="date">
-                    <input type="number" id="month" name="MM" placeholder="MM" required>
-                    <input type="number" id="day"   name="DD" placeholder="DD" required>
-                    <input type="number" id="year"  name="YYYY" placeholder="YYYY" required>
+                    <div class="col-md-7">
+                        <div class="email">
+                            <input type="email" id="email" name="email" placeholder="Your Email" required>
+                        </div>
+
+                        <button>
+                            <img src="<?php echo get_template_directory_uri().'/imgs/heart.svg' ?>" alt="heart" />
+                        </button>
+                    </div>
                 </div>
-
-                <div class="email">
-                    <input type="email" id="email" name="email" placeholder="Your Email" required>
-                </div>
-
             </form>
+        </div>
+
+        <div class="col-md-4"></div>
+        <div class="col-md-8">
+            <div class="health text-left">
+                <span class="blue">Your family's </span> <span class="green">health</span> 
+                <span class="blue">is important to us</span>
+            </div>
         </div>
 
     </div>
