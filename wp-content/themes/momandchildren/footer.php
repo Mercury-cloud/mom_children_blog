73e70daf4fb4bf4790366d449d6ee1484e09bc20
Mercury <drydev033@gmail.com>
2019-11-03 04:53:19 +0200

    Finished homepage normal design now mobile design

--- wp-content/themes/momandchildren/footer.php
+++ wp-content/themes/momandchildren/footer.php
@@ -1,3 +1,21 @@
+        
+        
+        <!-- chat box -->
+        <div id="chat">
+            <div class="chat_box"></div>
+            <div class="chat_picker">
+                <span class="chat_icon">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/chat/chat_pink.svg' ?>" alt="Chat">
+                </span>
+                <span class="text">
+                    Chat
+                </span>
+                <span class="chat_arrow">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/chat/chat_arrow.svg' ?>" alt="Chat Arrow">
+                </span>
+            </div>
+        </div>
+        
         <footer class="footer">
             <div class="row">
                 <div class="col-md-4">
@@ -5,18 +23,18 @@
                     <div class="footer_logo">
                         <h3>The Healthy Family</h3>
                         <div class="logo">
-                            <img src="<?php echo get_template_directory_uri().'/imgs/logo.png' ?>" alt="Mom And Children">
+                            <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/logo.png' ?>" alt="Mom And Children">
                         </div>
 
                         <div class="social">
-                            <a href="#" target="_blank">
-                                <img src="<?php echo get_template_directory_uri().'/imgs/facebook.png' ?>" alt="Facebook">
+                            <a href="#" class="facebook" target="_blank">
+                                <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/facebook.png' ?>" alt="Facebook">
                             </a>
-                            <a href="#" target="_blank">
-                                <img src="<?php echo get_template_directory_uri().'/imgs/instagram.svg' ?>" alt="Instagram">
+                            <a href="#" class="instagram" target="_blank">
+                                <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/instagram.svg' ?>" alt="Instagram">
                             </a>
-                            <a href="#" target="_blank">
-                                <img src="<?php echo get_template_directory_uri().'/imgs/youtube.png' ?>" alt="Youtube">
+                            <a href="#" class="youtube" target="_blank">
+                                <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/youtube.png' ?>" alt="Youtube">
                             </a>
                         </div>
 
@@ -30,32 +48,42 @@
                 <div class="col-md-8">
                     <div class="row">
                         <div class="col-md-6">
-                            <div class="week_by_week_footer">
-                                <img src="<?php echo get_template_directory_uri().'/imgs/pergenancy_icon.png' ?>" alt="Week By Week">
+                            <a class="week_by_week_footer" href="">
+                                <img class="img-responsive" src="<?php echo get_template_directory_uri().'/imgs/footer/week_by_week.svg' ?>" alt="Week By Week">
                                 <p>WEEK BY WEEK</p>
                                 <h2>Pregnancy Calendar</h2>
+                            </a>
+                            <div class="copy_rights">
+                                © 2019 momandchildreN. All Rights Reserved.
                             </div>
                         </div>
 
                         <div class="col-md-6">
-                            <ul class="list-unstyled">
-                                <li><a href="/about">About</a></li>
-                                <li><a href="/press">Press</a></li>
-                                <li><a href="/contact">Contact</a></li>
-                            </ul>
+                            <div class="links">
+                                <ul class="list-unstyled">
+                                    <li><a href="/about">About</a></li>
+                                    <li><a href="/contact">Contact</a></li>
+                                </ul>
+                            </div>
 
-                            <div class="talk">
-                                <h5>AFFILIATE RELATIONSHIPS</h5>
-                                <p>We have affiliate revenue relationships with Amazon, and with other retailers that are handled by third parties like Skimlinks, RewardStyle, Commission Junction and Linkshare. If you see a link to a retailer, please assume that it is an affiliate link. However, rest assured that our affiliate relationships do not guide our product recommendations, at all.</p>
+
+                            <div class="contact">
+                                <form action="/send-contact-message">
+                                    <div class="row">
+                                        <div class="col-md-6 clas1">
+                                            <textarea name="message" id="message" placeholder="Your Message" required></textarea>
+                                        </div>
+                                        <div class="col-md-6 clas2">
+                                            <input type="text" name="name" placeholder="Your Name" class="name" required>
+                                            <input type="email" name="email" placeholder="Your Email" class="email" required>
+                                            <input type="submit" value="Send Message" class="submit" name="send_message">
+                                        </div>
+                                    </div>
+
+                                </form>
                             </div>
 
                         </div>
-                    </div>
-
-                    
-
-                    <div class="copy_rights">
-                        © 2019 momandchildreN. All Rights Reserved. Terms and Conditions | Privacy Policy | Site by CooperHouse
                     </div>
                 </div>
 
